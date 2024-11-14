46319895bf376d66a61a638bdb3b83fc1bcdd709
Vitaliy <vitaliy.yanitskyy@gmail.com>
2024-11-14 15:09:27 +0200

    Add simple register

---
+++ src/Pages/Register.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+
+function Register() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+
+    const handleSubmit = async (e) => {
+        try {
+            fetch('http://localhost:8080/api/Auth/register', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    username,
+                    password,
+                    confirmPassword,
+                }),
+            });
+            alert('User registered successfully');
+        } catch (error) {
+            console.error(error);
+            alert('An error occurred');
+        }
+    };
+
+    return (
+        <div>
+            <h1>Register</h1>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+                <input
+                    type="password"
+                    placeholder="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <input
+                    type="password"
+                    placeholder="confirm password"
+                    value={confirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                />
+                <button type="submit">Register</button>
+            </form>
+        </div>
+    );
+}
+
+export default Register;
